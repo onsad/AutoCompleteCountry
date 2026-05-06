358c36330c4b1f5ca3cef152a23c0d3ff546467b
onsad <ondrej.sadilek@gmail.com>
2026-05-06 18:11:48 +0200

    Data generator

---
+++ CountryGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Text;
+using System.Text.Json;
+
+string jsonFilePath = Path.Combine(
+    Environment.CurrentDirectory,
+    "Source",
+    "countries.json"
+);
+string jsonString = File.ReadAllText(jsonFilePath);
+var countries = JsonSerializer.Deserialize<Class1[]>(jsonString);
+
+var insertSql = new StringBuilder("INSERT INTO Country\r\n  ( CountryName, CountryCode, Currency )\r\nVALUES");
+
+foreach (var country in countries)
+{
+    var currency = string.Empty;
+    var curr = country.currencies.GetType().GetProperties().Where(val => val != null);
+
+    foreach (PropertyInfo pi in country.currencies.GetType().GetProperties())
+    {
+        object value = pi.GetValue(country.currencies, null);
+
+        if (value != null && !string.IsNullOrEmpty(value.ToString()))
+        {
+            currency = value.ToString();
+        }
+    }
+
+    insertSql.AppendLine($"  ( '{country.name.official}', '{country.cca3}', '{currency}' ),");
+}
+
+
+File.WriteAllText(Path.Combine(Environment.CurrentDirectory, "export.sql"), insertSql.ToString());
